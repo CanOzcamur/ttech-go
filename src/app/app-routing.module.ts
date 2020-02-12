c7190e55f281eba125d149d5b8fb06455f373444
Cemre Tok <cemreto@gmail.com>
2020-02-12 13:19:37 +0300

    İnitialize Project ttech-Go

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { PackagePageComponent } from './pages/package-page/package-page.component';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
+import { PageNotFoundPageComponent } from './pages/page-not-found-page/page-not-found-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'user', pathMatch: 'full'},
+  { path: 'user', component: UserPageComponent },
+  { path: 'package', component: PackagePageComponent },
+  { path: 'checkout', component: CheckoutPageComponent },
+  { path: '**', component: PageNotFoundPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [UserPageComponent, PackagePageComponent, CheckoutPageComponent, PageNotFoundPageComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { PackagePageComponent } from './pages/package-page/package-page.component';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
+import { PageNotFoundPageComponent } from './pages/page-not-found-page/page-not-found-page.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserPageComponent,
+    PackagePageComponent,
+    CheckoutPageComponent,
+    PageNotFoundPageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
