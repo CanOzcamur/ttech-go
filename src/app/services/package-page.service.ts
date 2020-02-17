be9268ef6d8be75952c1a84f3879c1eb9d69308d
ibrahim <ibrahim.erdogan@bulutmd.com>
2020-02-17 11:24:43 +0300

    All css were brought together.

--- src/app/services/package-page.service.ts
+++ src/app/services/package-page.service.ts
@@ -11,7 +11,7 @@ export class PackagePageService {
   constructor(private httpClient: HttpClient) { }
 
   getProduct(phoneNumber:string): Observable<Product> {
-    return this.httpClient.post<Product>("/api/inquiryPackages?msisdn=" + phoneNumber, { title: 'Angular POST Request Example' });
-    //return this.httpClient.get<Product>("http://www.mocky.io/v2/5e4534b33000005d3561499a");
+    //return this.httpClient.post<Product>("/api/inquiryPackages?msisdn=" + phoneNumber, { title: 'Angular POST Request Example' });
+    return this.httpClient.get<Product>("http://www.mocky.io/v2/5e4534b33000005d3561499a");
   }
 }
